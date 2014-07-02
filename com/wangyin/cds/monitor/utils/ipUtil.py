782063e7d8f38c544f8fb6e33acd64b0ff9b8c76
saimasoft <saima123>
2014-07-02 15:01:57 +0800

    monitor agent 代码调整

--- com/wangyin/cds/monitor/utils/ipUtil.py
+++ com/wangyin/cds/monitor/utils/ipUtil.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 __author__ = 'wylitu'
-#import fcntl
+import fcntl
 import socket,platform,struct
 
 class IpUtil:
@@ -10,12 +10,12 @@ class IpUtil:
          if(sys =="Windows"):
               myname = socket.getfqdn(socket.gethostname())
               return socket.gethostbyname(myname)
-         #elif(sys == "Linux"):
-        #     ifname ='eth0'
-        #     skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #     pktString = fcntl.ioctl(skt.fileno(), 0x8915, struct.pack('256s', ifname[:15]))
-        #     ipString  = socket.inet_ntoa(pktString[20:24])
-        #      return ipString
+         elif(sys == "Linux"):
+              ifname ='eth0'
+              skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+              pktString = fcntl.ioctl(skt.fileno(), 0x8915, struct.pack('256s', ifname[:15]))
+              ipString  = socket.inet_ntoa(pktString[20:24])
+              return ipString
          return None
 if __name__ == '__main__':
     print IpUtil().get_local_ip()
